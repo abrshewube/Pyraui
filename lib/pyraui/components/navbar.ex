0e424100b4bd3ce22fe770a7ff3f5186ff93ecb2
James Gough <james@peacefulprogramming.dev>
2025-12-14 03:38:04 +0000

    put dark bg on navbar

--- lib/pyraui/components/navbar.ex
+++ lib/pyraui/components/navbar.ex
@@ -104,7 +104,7 @@ defmodule Pyraui.Components.Navbar do
                 [
                   "block pl-3 pr-4 py-2 border-l-4 text-base font-medium transition-colors",
                   if(active?,
-                    do: "bg-blue-50 border-blue-500 text-blue-700",
+                    do: "bg-blue-50 dark:bg-blue-900 border-blue-500 text-blue-700",
                     else: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300"
                   )
                 ]
